77475d4b6c2c4d538d49aa4685c556602419d288
Taher Maimoon <tmaimoon18@gmail.com>
2024-02-26 23:51:46 +0530

    Created WelcomeActivity Design

---
+++ app/src/main/java/com/example/wechat/WelcomeActivity.java
@@ -0,0 +1,17 @@
+package com.example.wechat;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class WelcomeActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+}
