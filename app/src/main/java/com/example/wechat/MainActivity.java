f3d64cb3729c20080d4138c59f0242a04f203145
Taher Maimoon <tmaimoon18@gmail.com>
2024-02-29 12:08:50 +0530

    Designed contacts_row

--- app/src/main/java/com/example/wechat/MainActivity.java
+++ app/src/main/java/com/example/wechat/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.wechat;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.wechat.adapter.ViewPagerAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private TabLayout tabLayout;
     private ViewPager viewPager;
     private ViewPagerAdapter adapter;
+    private FloatingActionButton floatBtnContacts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +25,19 @@ public class MainActivity extends AppCompatActivity {
 
         tabLayout = findViewById(R.id.tabLayout);
         viewPager = findViewById(R.id.viewPager);
+        floatBtnContacts = findViewById(R.id.floatBtnContacts);
 
         adapter = new ViewPagerAdapter(getSupportFragmentManager(), MainActivity.this);
         viewPager.setAdapter(adapter);
 
         tabLayout.setupWithViewPager(viewPager);
+
+        floatBtnContacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ContactsActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
