bd1c0bf3d90e3b797f204b8bfb3782596d040cee
Taher Maimoon <tmaimoon18@gmail.com>
2024-03-25 02:26:18 +0530

    Created and designed ChatActivity

---
+++ app/src/main/java/com/example/letschat/view/chat/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.example.letschat.view.chat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.letschat.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
